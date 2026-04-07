9de714a3d003f6d342371e67396ed7e9f1277f3b
Waleed-creator <mwaleed22102645@example.com>
2026-04-08 01:35:47 +0500

    Firebase CLi setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mindshield/my_app.dart';
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
-// import 'package:iconsax/iconsax.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
 
-
-//  Hello Malik
